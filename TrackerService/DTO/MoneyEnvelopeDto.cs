d01f4660416ce37bcfa0b2a06c08ed76c105deb2
migimasa <migimasa@gmail.com>
2018-04-29 18:57:35 -0500

    Add DTO representing an envelope

---
+++ TrackerService/DTO/MoneyEnvelopeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackerService.DTO
+{
+    public class MoneyEnvelopeDto
+    {
+        public string Name { get; set; }
+        public decimal StartingBalance { get; set; }
+        public decimal AvailableBalance { get; set; }
+    }
+}
