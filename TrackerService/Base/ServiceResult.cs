8b6423f3d4647035d701b092f5a040875a3b8550
migimasa <migimasa@gmail.com>
2018-04-29 18:57:56 -0500

    add Result object for changes in a service layer

---
+++ TrackerService/Base/ServiceResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace TrackerService.Base
+{
+    public class ServiceResult
+    {
+        public bool IsSuccess
+        {
+            get { return Errors.Count == 0; }
+        }
+
+        public List<string> Errors { get; set; } = new List<string>();
+        
+        public object Data { get; set; }
+
+        public void AddErrorMessage(string error)
+        {
+            Errors.Add(error);
+        }
+    }
+}
