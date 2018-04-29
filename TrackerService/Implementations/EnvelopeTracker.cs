479590c9a88e173dac1c4c0847aca3c24fe8ddbb
migimasa <migimasa@gmail.com>
2018-04-29 18:57:08 -0500

    Add implementation for envelope tracking

---
+++ TrackerService/Implementations/EnvelopeTracker.cs
@@ -0,0 +1,23 @@
+﻿using TrackerService.Base;
+using TrackerService.DTO;
+using TrackerService.Interfaces;
+
+namespace TrackerService.Implementations
+{
+    public class EnvelopeTracker : IEnvelopeTracker
+    {
+        public ServiceResult CreateEnvelope(string envelopeName, decimal startingBalance)
+        {
+            ServiceResult result = new ServiceResult();
+
+            result.Data = new MoneyEnvelopeDto()
+            {
+                Name = envelopeName,
+                StartingBalance = startingBalance,
+                AvailableBalance = startingBalance
+            };
+            
+            return result;
+        }
+    }
+}
