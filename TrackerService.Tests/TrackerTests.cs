48c21cdc5520ebc26cd89a5a1c29e184af0f6e5b
migimasa <migimasa@gmail.com>
2018-04-30 06:24:35 -0500

    Refactor to share service creation code

--- TrackerService.Tests/TrackerTests.cs
+++ TrackerService.Tests/TrackerTests.cs
@@ -1,9 +1,14 @@
 using System;
+using AutoMapper;
 using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Tracker.DAL.Interfaces;
 using TrackerService.Base;
 using TrackerService.DTO;
 using TrackerService.Interfaces;
 using TrackerService.Implementations;
+using TrackerService.Mappings;
+using TrackerService.Tests.Mocks;
 using Xunit;
 
 namespace TrackerService.Tests
@@ -19,7 +24,8 @@ namespace TrackerService.Tests
         [Fact]
         public void CreateNewEnvelope_ValidInputs()
         {
-            IEnvelopeTracker tracker = new EnvelopeTracker();
+            IEnvelopeRepository envelopeRepo = new MockEnvelopeRepository();
+            var tracker = CreateEnvelopeTrackerService(envelopeRepo);
 
             string envelopeName = "TEST_ENVELOPE";
             decimal startingBalance = 100.00M;
@@ -39,5 +45,43 @@ namespace TrackerService.Tests
             envelope.StartingBalance.Should().Be(startingBalance);
             envelope.AvailableBalance.Should().Be(startingBalance);
         }
+
+        private IEnvelopeTracker CreateEnvelopeTrackerService(IEnvelopeRepository envelopeRepo)
+        {
+            IEnvelopeTracker tracker = new EnvelopeTracker(new NullLogger<EnvelopeTracker>(),
+                                                           this.CreateMapper(),
+                                                           envelopeRepo);
+            return tracker;
+        }
+
+        private IMapper CreateMapper()
+        {
+            var mapConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<EnvelopeMappingProfile>();
+            });
+
+            return mapConfig.CreateMapper();
+        }
+
+//        [Fact]
+//        public void CreateNewEnvelope_InvalidName_NotCreated()
+//        {
+//            IEnvelopeTracker tracker = new EnvelopeTracker();
+//
+//            string envelopeName = "";
+//            decimal startingBalance = 100.00M;
+//
+//            ServiceResult createResult = tracker.CreateEnvelope(envelopeName, startingBalance);
+//
+//            createResult.Should().NotBeNull();
+//            createResult.IsSuccess.Should().BeFalse();
+//            createResult.Errors.Should().NotBeNull();
+//            createResult.Errors.Count.Should().Be(1);
+//            createResult.Errors[0].Should().Be("Envelope must have a name.");
+//            createResult.Data.Should().BeNull();
+//
+//        }
+        
     }
 }
