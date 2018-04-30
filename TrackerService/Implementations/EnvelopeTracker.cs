bc74dc99b003e2f8f6e6d6d621d005a3cd54ec5e
migimasa <migimasa@gmail.com>
2018-04-30 06:23:46 -0500

    Add constructor to pass in dependencies

--- TrackerService/Implementations/EnvelopeTracker.cs
+++ TrackerService/Implementations/EnvelopeTracker.cs
@@ -1,4 +1,7 @@
-﻿using TrackerService.Base;
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Tracker.DAL.Interfaces;
+using TrackerService.Base;
 using TrackerService.DTO;
 using TrackerService.Interfaces;
 
@@ -6,6 +9,19 @@ namespace TrackerService.Implementations
 {
     public class EnvelopeTracker : IEnvelopeTracker
     {
+        private readonly ILogger<EnvelopeTracker> logger;
+        private readonly IMapper mapper;
+        private readonly IEnvelopeRepository envelopeRepository;
+
+        public EnvelopeTracker(ILogger<EnvelopeTracker> logger,
+            IMapper mapper,
+            IEnvelopeRepository envelopeRepo)
+        {
+            this.logger = logger;
+            this.mapper = mapper;
+            this.envelopeRepository = envelopeRepo;
+        }
+        
         public ServiceResult CreateEnvelope(string envelopeName, decimal startingBalance)
         {
             ServiceResult result = new ServiceResult();
