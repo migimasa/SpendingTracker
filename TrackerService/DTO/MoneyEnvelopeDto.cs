054be9852457c73611f3964cfa8bf5eac23e9c7a
migimasa <migimasa@gmail.com>
2018-05-06 07:54:04 -0500

    Add id to envelope

--- TrackerService/DTO/MoneyEnvelopeDto.cs
+++ TrackerService/DTO/MoneyEnvelopeDto.cs
@@ -2,6 +2,7 @@
 {
     public class MoneyEnvelopeDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal StartingBalance { get; set; }
         public decimal AvailableBalance { get; set; }
