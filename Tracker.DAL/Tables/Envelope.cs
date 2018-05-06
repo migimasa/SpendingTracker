f496227709529de52011ffaa22992da9031963da
migimasa <migimasa@gmail.com>
2018-05-06 07:53:44 -0500

    Add envelope table class to dal

---
+++ Tracker.DAL/Tables/Envelope.cs
@@ -0,0 +1,10 @@
+﻿namespace Tracker.DAL.Tables
+{
+    public class Envelope
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal StartingBalance { get; set; }
+        public decimal AvailableBalance { get; set; }
+    }
+}
