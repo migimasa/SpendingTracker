0846c421049847233a840ddeaf37077078d592a9
migimasa <migimasa@gmail.com>
2018-05-09 22:12:52 -0700

    add mappings for table to dto and back

--- TrackerService/Mappings/EnvelopeMappingProfile.cs
+++ TrackerService/Mappings/EnvelopeMappingProfile.cs
@@ -1,9 +1,20 @@
 ﻿using AutoMapper;
+using Tracker.DAL.Tables;
+using TrackerService.Base;
+using TrackerService.DTO;
+using TrackerService.Enums;
 
 namespace TrackerService.Mappings
 {
     public class EnvelopeMappingProfile : Profile
     {
-        
+        public EnvelopeMappingProfile()
+        {
+            CreateMap<MoneyEnvelopeDto, Envelope>()
+                .ForMember(dest => dest.RolloverType, opts => opts.MapFrom(src => src.RolloverType.Value));
+            CreateMap<Envelope, MoneyEnvelopeDto>()
+                .ForMember(dest => dest.RolloverType,
+                    opts => opts.MapFrom(src => Enumeration.FromValue<BalanceRolloverType>(src.RolloverType)));
+        }
     }
 }
