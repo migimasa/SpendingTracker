12968f6a550e2ce15fa53960032689cbe34ab40c
migimasa <migimasa@gmail.com>
2018-05-09 22:12:36 -0700

    add rollover type enum

---
+++ TrackerService/Enums/BalanceRolloverType.cs
@@ -0,0 +1,14 @@
+﻿using TrackerService.Base;
+
+namespace TrackerService.Enums
+{
+    public class BalanceRolloverType : Enumeration
+    {
+        public static readonly BalanceRolloverType Unknown = new BalanceRolloverType(0, "Unknown");
+        public static readonly BalanceRolloverType ApplyToSurplus = new BalanceRolloverType(1, "Stash");
+        public static readonly BalanceRolloverType Carryover = new BalanceRolloverType(2, "Carryover");
+        
+        private BalanceRolloverType() { }
+        private BalanceRolloverType(int value, string displayName) : base(value, displayName) { }
+    }
+}
