f7b125e3c27f01ab22f035ca204f0a1f9ff5c30f
migimasa <migimasa@gmail.com>
2018-05-09 22:14:11 -0700

    Add rollover type to create, add get method; add validation for envelopes

--- TrackerService/Implementations/EnvelopeTracker.cs
+++ TrackerService/Implementations/EnvelopeTracker.cs
@@ -1,10 +1,13 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Tracker.DAL.Interfaces;
 using Tracker.DAL.Tables;
 using TrackerService.Base;
 using TrackerService.DTO;
+using TrackerService.Enums;
 using TrackerService.Interfaces;
+using TrackerService.Mappings;
 
 namespace TrackerService.Implementations
 {
@@ -23,25 +26,34 @@ namespace TrackerService.Implementations
             this.envelopeRepository = envelopeRepo;
         }
 
-        public ServiceResult CreateEnvelope(string envelopeName, decimal startingBalance)
+        public ServiceResult CreateEnvelope(string envelopeName, decimal startingBalance,
+            BalanceRolloverType rolloverType)
         {
             ServiceResult result = ValidateEnvelopeInformation(envelopeName, startingBalance);
 
             if (result.IsSuccess)
             {
-                result.Data = new MoneyEnvelopeDto()
+                MoneyEnvelopeDto envelope = new MoneyEnvelopeDto()
                 {
                     Name = envelopeName,
                     StartingBalance = startingBalance,
-                    AvailableBalance = startingBalance
+                    AvailableBalance = startingBalance,
+                    RolloverType = rolloverType
                 };
 
-                Envelope envelopeRecord = mapper.Map<Envelope>(result.Data);
-                
-                
+                int envelopeId = envelopeRepository.SaveEnvelope(mapper.Map<Envelope>(envelope));
+                if (envelopeId > 0)
+                    result.Data = envelopeId;
+                else
+                    result.AddErrorMessage("Could not save envelope");
             }
 
             return result;
+        }
+
+        public MoneyEnvelopeDto GetEnvelope(int id)
+        {
+            return mapper.Map<MoneyEnvelopeDto>(envelopeRepository.GetEnvelopeById(id));
         }
 
         private ServiceResult ValidateEnvelopeInformation(string envelopeName, decimal startingBalance)
@@ -50,8 +62,22 @@ namespace TrackerService.Implementations
 
             if (string.IsNullOrWhiteSpace(envelopeName))
                 result.AddErrorMessage("Envelope must have a name.");
+            else if (IsNameInUse(envelopeName))
+                result.AddErrorMessage($"{envelopeName} is already in use. Please select another name.");
+            
+            if (startingBalance <= 0)
+                result.AddErrorMessage("Starting balance must be positive.");
+            
+            
             
             return result;
+        }
+
+        private bool IsNameInUse(string envelopeName)
+        {
+            bool isNameInUse = envelopeRepository.GetAllEnvelopes().Select(env => env.Name).Contains(envelopeName);
+
+            return isNameInUse;
         }
     }
 }
--- TrackerService/Interfaces/IEnvelopeTracker.cs
+++ TrackerService/Interfaces/IEnvelopeTracker.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using TrackerService.Base;
+using TrackerService.DTO;
+using TrackerService.Enums;
 
 namespace TrackerService.Interfaces
 {
     public interface IEnvelopeTracker
     {
-        ServiceResult CreateEnvelope(string envelopeName, decimal startingBalance);
+        ServiceResult CreateEnvelope(string envelopeName, decimal startingBalance, BalanceRolloverType rolloverType);
+        MoneyEnvelopeDto GetEnvelope(int id);
     }
 }
